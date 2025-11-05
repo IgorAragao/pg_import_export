f86e5d42a1cfce4037803dca984901bf8207b1ac
IgorAragao <igor_lamino@hotmail.com>
2025-11-05 12:41:15 -0300

    Adicionado entrada de parâmetros e melhora nos logs de execução

--- export_data_postgres.py
+++ export_data_postgres.py
@@ -1,33 +1,73 @@
 import psycopg2
+import sys
 
 
-TABLE_NAME = '' # Tabela origem para exportar dados
-DIR_PATH = '' # Caminho para a criação do arquivo exportado
+## Valida os parâmetros
+if len(sys.argv) <= 2:
+    print('Argumentos insuficientes!')
+    sys.exit()
+
+
+## Tabela e schema para exportar os dados (ex: public.clientes)
+TABLE_NAME = sys.argv[1]
+
+## Caminho do diretorio onde o arquivo será salvo e o nome do arquivo
+DIR_PATH = sys.argv[2] #(ex: ~/export_tables/clientes.copy)
+
+## Tipo do arquivo que será exportado
+TYPE_ARCHIVE = sys.argv[3] #(ex: CSV, BINARY)
+
 
 def conn_postgres():
 
     try:
         conn = psycopg2.connect(
-                host="<ip>",
-                port="<port>",
-                database="<db>",
-                user="<user>"
+                ## ToDo: ajustar conexão para funcionar via parametro sys.argv
+                host="127.0.0.1",
+                port="5432",
+                database="apollo",
+                user="postgres"
         )
     except Exception as err:
-        print(f"Error: {err}")
+        print(f"[ERROR] ❌ {err}")
     
     return conn
 
+
+def conn_close(conn):
+
+    if conn == None:
+       print("[ERROR] ❌ Conexão não estabelecida!")
+    else:
+      print("[INFO] Conexão encerrada!")
+      conn.close()  
+
+
+def cursor_execute(conn):
+
+    cursor = conn.cursor()
+    cursor.execute(f"COPY {TABLE_NAME} TO '{DIR_PATH}' WITH (FORMAT {TYPE_ARCHIVE})")
+    conn.commit() 
+    cursor.close()
+    print(f"[INFO] 🚀 EXPORT COPY realizado com sucesso para a tabela: {TABLE_NAME}.")
+
+
 def export_table():
 
-    cursor = conn_postgres().cursor()
-    cursor.execute(f"COPY {TABLE_NAME} TO '{DIR_PATH}' WITH (FORMAT CSV)")
+    conn = None
+    try:
+        conn  = conn_postgres()
+        if conn != None:
+           cursor_execute(conn)
+    except:
+        print(f"[ERROR] ❌ Falha ao realizar EXPORT COPY para a tabela: {TABLE_NAME}.")
+    finally:
+        conn_close(conn)
 
 
 def main():
 
     export_table()
-
 
 
 if __name__ == "__main__":
